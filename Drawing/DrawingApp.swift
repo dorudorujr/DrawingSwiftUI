0b6a3648e3042236e482ff1a30abe0ae2fbcad57
seiya-sugioka <seiya_sugioka@waku-2.com>
2021-10-19 23:26:56 +0900

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by RLS77777777 on 2021/10/19.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
